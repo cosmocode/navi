60fc9a6909441c8dff2cc9eca380f459ce2004ab
Michael Große <grosse@cosmocode.de>
2015-02-18 16:17:53 +0100

    Fix style issues
      * Do not use post-increment unless there is a specific reason for doing
    so.
      * Update function signature in order to comply with strict standards

--- action.php
+++ action.php
@@ -17,7 +17,7 @@ class action_plugin_navi extends DokuWiki_Action_Plugin {
     /**
      * plugin should use this method to register its handlers with the dokuwiki's event controller
      */
-    function register(&$controller) {
+    function register(Doku_Event_Handler $controller) {
         $controller->register_hook('PARSER_CACHE_USE','BEFORE', $this, 'handle_cache_prepare');
     }
 
